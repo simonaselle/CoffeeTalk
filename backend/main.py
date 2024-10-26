686108a6893158015df7b89272c7cb81a4b35acd
Simona Sellecchia <sellecchiasimona@gmail.com>
2024-10-26 15:47:31 +0000

    ran localhost 3001

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware #can call this server from any other website 
+
 from pydantic import BaseModel
 
 app = FastAPI()
