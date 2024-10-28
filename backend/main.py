6f0e68349bd0a2a1dfdf24a8aac9258d8332d5d3
Simona Sellecchia <sellecchiasimona@gmail.com>
2024-10-28 21:01:30 +0000

    created a function to save the user data to the file

--- backend/main.py
+++ backend/main.py
@@ -30,6 +30,11 @@ def hash_password(password: str):
 def verify_password(password: str, hashed_password: str):
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
 
+# creating a function to save the user data in the file
+def save_user(user: User):
+    with open(usersFile, "a") as f: #open the file in append; append mode is used to add the data to the end of the file
+        f.write(f"{user.username},{hash_password(user.password).decode()}\n") # write the username and hashed password in the file
+
 @app.post("/authenticate")
 async def authenticate(user: User):
     # Save the user data in the in-memory storage
