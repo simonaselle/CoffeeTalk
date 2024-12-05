d59e722ec1ffd7d0a1439110f68ecb525af2de62
Daniel <dhodowanec@arcadia.edu>
2024-12-05 14:50:14 -0500

    cleaning repo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AuthPage from './AuthPage';
 import ChatsPage from './ChatsPage';
 import AboutPage from './AboutPage'; // Import the AboutPage component
+import LoginForm from './LoginForm';
 import './App.css';
 
 function App() {
@@ -11,17 +12,11 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={
-            !user ? (
-              <AuthPage onAuth={(user) => setUser(user)} />
-            ) : (
-              <ChatsPage user={user} />
-            )
-          }
-        />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/chat" element={<ChatsPage user={user} />} />
         <Route path="/about" element={<AboutPage />} />
+        {/* Redirect any other route to login */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
   );
